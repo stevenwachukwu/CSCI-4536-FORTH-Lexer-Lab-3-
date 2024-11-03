1efec288115d05f21d8b67b8b483a4049cd4855f
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-11-03 18:12:14 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "Lexer.hpp"
 using namespace std;
 
-int main() {
+//USE: ./main inputFile.f
+int main(int argc, char* argv[]) {
+    try{
+	if(argv[1]){
+		string inFileName = argv[1];
+		Lexer lexer(inFileName);
+		lexer.doLex();
+		lexer.printTokens();
+	}else{
+		cout << "Error: Input file argument not provided." << endl;
+	}
+    }
+    catch(const char* e){
+	cerr << "\nCaught exception: " << e << "\n";
+    }
+    return 0;
+
 }
