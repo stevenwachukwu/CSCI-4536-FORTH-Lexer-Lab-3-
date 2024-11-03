c8ef02b5e490b8cbc856075275173e52362bf6a1
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-11-03 18:09:19 -0500

    Update Token.hpp

--- Token.hpp
+++ Token.hpp
@@ -1,10 +1,12 @@
 //Written by Steve Nwachukwu and Tyler Burzenski
 
+#pragma once
 #ifndef TOKEN_HPP
 #define TOKEN_HPP
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 #include "Types.hpp"
 
 using namespace std;
@@ -16,6 +18,7 @@ private:
     int refCount; //initialized to be 1 later
 
 public:
+  Token() = default;
   Token(string name, TokenType type);
   ostream& print(ostream& out) const;
   void incrementRefCounter();
