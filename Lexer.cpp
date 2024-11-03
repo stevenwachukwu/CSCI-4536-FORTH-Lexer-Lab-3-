c3b09062844c654bce0860161c7fb47a2cef43ab
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-11-03 18:04:23 -0500

    Update Lexer.cpp

--- Lexer.cpp
+++ Lexer.cpp
@@ -21,12 +21,102 @@ Lexer::~Lexer(){
     outputFile.close();
 }
 
-Lexer::doLex(){} //Tyler's part
+void Lexer::doLex(){ 
+    while(inputFile.get(currentChar)) {
+        switch( state ){
+	    case START: doStart(); break;
+
+	    case SLASH_PENDING: 
+		if( currentChar == ' ' ){
+		    state = ACQUIRING_SLASH;
+		}
+		else{
+		    state = ACQUIRING_TOKEN;
+		}
+		break;
+
+	    case ACQUIRING_SLASH:
+		outputFile << currentChar;
+		do{
+		    inputFile.get(currentChar);
+		    outputFile << currentChar;		//OUTPUT TO FILE
+		}while(currentChar != '\n');
+		state = START;
+		break;
+
+	    case PAREN_PENDING:
+		if( currentChar == ' ' ){
+		    state = ACQUIRING_PAREN;
+		} 
+		else{
+		    state = ACQUIRING_TOKEN;
+		}
+		break;
+
+	    case ACQUIRING_PAREN:
+		cout << "( ";
+		do{
+		    cout << currentChar;
+		    inputFile.get(currentChar);
+		} while(currentChar != ')');
+		state = START;
+		cout << " )\n";
+		break;
+
+	    case ACQUIRING_TOKEN:
+	    {
+		while(currentChar != ' ' && currentChar != '\n'){			// Token builder
+		    tokenString += currentChar;
+		    inputFile.get(currentChar);
+		}
+
+		if(tokenString == ".\""){		// String check
+		    doToken(tokenString, WORD);		// WORD init
+		    state = ACQUIRING_STRING;
+		    break;
+		}
+
+		bool isNum = true;			// Num check
+		for(char target : tokenString){
+		    if(target < '0' || target > '9'){
+			isNum = false;
+			break;
+		    }
+		}					// Num set
+		if(isNum == true){
+		    doToken(tokenString, NUMBER);	// NUMBER init
+		    state = START;
+		    break;
+		}
+		else{
+		    doToken(tokenString, WORD);			// Default set
+		    state = START;
+		    break;
+		}
+	    }
+	    case ACQUIRING_STRING:
+		
+		while(currentChar != '\"'){
+		    tokenString += currentChar;
+		    inputFile.get(currentChar);
+		}
+		doToken(tokenString, STRING); 
+		state = START;
+		break;
+
+	    default:
+		cout << "Could not read state :("; break;
+	}
+    }
+}
 
 void Lexer::printTokens(){
-    outputFile << "The Symbol Table:\n";
+    outputFile << "\nThe Symbol Table:\n" << endl;
+    outputFile << "Token Name" << "\t\t\tType" << "\tReference(s)" << endl;
     for (const auto& pair : tokenMap) {
+//	outputFile << pair.first << "\t";
         pair.second.print(outputFile);
+	outputFile << "\n";
     }
 }
 
@@ -36,27 +126,29 @@ void Lexer::doToken(string name, TokenType tt) {
         it ->second.incrementRefCounter();
     }
     else {
-        Token newToken(name, tt);
+        Token newToken =  Token(name, tt);
         tokenMap[name] = newToken;
     }
     tokenString = ""; //resets the token string after processing
 } //helper function for doToken
 
 void Lexer::doStart() {
-switch (currentChar) {
-    case ' ':
-    case '\t':
-    case '\n':
-     //case statements deal with spacing, newlines, and tabs
-        break;
-    case '\\':
-        type = SLASH_PENDING;
-        break;
-    case '(':
-        type = PAREN_PENDING;
-    default:
-        type = ACQUIRING_TOKEN;
-        tokenString += currentChar;
-        break;
+    switch (currentChar) {
+        case ' ':
+        case '\t': 
+        case '\n':
+         //case statements deal with spacing, newlines, and tabs
+            break;
+        case '\\':
+            state = SLASH_PENDING;
+            break;
+         case '(':
+            state = PAREN_PENDING;
+	    break;
+        default:
+            state = ACQUIRING_TOKEN;
+            tokenString += currentChar;
+            break;
     }
 }
+
