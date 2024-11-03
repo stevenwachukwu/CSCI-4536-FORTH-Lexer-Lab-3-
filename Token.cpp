9eeead5b9c99ac32b7f95de063907362fde99d8d
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-11-03 18:10:02 -0500

    Update Token.cpp

--- Token.cpp
+++ Token.cpp
@@ -1,11 +1,29 @@
 //Written by Steve Nwachukwu and Tyler Burzenski
 
 #include "Token.hpp"
+#include "Lexer.hpp"
 
 Token::Token(string name, TokenType type): name(name), type(type), refCount(1) {}
 
 ostream& Token::print(ostream& out) const {
-    cout << "Token Name: " << name << "| Type: " << type << "| Reference(s): " << refCount;
+    string typePrint;
+    switch(type){		//enum translator
+	case WORD:
+	    typePrint = "Word";
+	    break;
+	case NUMBER:
+	    typePrint = "Number";
+	    break;
+	case STRING:
+	    typePrint = "String";
+	    break;
+	default:
+	    typePrint = "Unknown";
+	    break;
+    }
+    out << left << setw(35) << name << setw(15) << typePrint << setw(10) << refCount; 
+    typePrint = "";
+    return out;
 } //definition for the print function
 
 void Token::incrementRefCounter() {
